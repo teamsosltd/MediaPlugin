41690d046b17052e6d7bbde85a00792bc6c6a3a9
alexfelton <alex@alexfelton.com>
2023-01-11 15:51:49 +0000

    Android 13 permission fixes

--- src/Media.Plugin/Android/CustomPermissions.cs
+++ src/Media.Plugin/Android/CustomPermissions.cs
@@ -5,13 +5,16 @@ using static Xamarin.Essentials.Permissions;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Android;
+using Android.OS;
 
 namespace Plugin.Media
 {
 	public class StoragePermission : BasePlatformPermission
 	{
 		public override (string androidPermission, bool isRuntime)[] RequiredPermissions =>
-				new (string, bool)[] { (Manifest.Permission.WriteExternalStorage, true),
+            Build.VERSION.SdkInt >= BuildVersionCodes.Tiramisu
+            ? new (string, bool)[] {(Manifest.Permission.ReadMediaImages, true)}
+            : new (string, bool)[] { (Manifest.Permission.WriteExternalStorage, true),
 				(Manifest.Permission.ReadExternalStorage, true)};
 	}
 }
